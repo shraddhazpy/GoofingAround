b7585eed764e9a0a5550dd05230e596f63e17e6e
shraddhazpy <shraddha.gaga@gmail.com>
2020-08-08 19:20:38 +0530

    Some bare bones Exception handling

--- GetMyStuff.py
+++ GetMyStuff.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 import regex as re
+from FnF import FileNotFound
 
 URL = 'https://allpoetry.com/'
 
@@ -11,11 +12,15 @@ def get_me_my_stuff(ep):
     soup= BeautifulSoup(source,'lxml')
     reg_text= re.compile('orig_.*')
 
-    with open('composition.txt','w') as f:
-        for i in soup.find_all('div',{"class" : 'fonted'}):
-            f.write(f"{i.a.text.upper()}\n")
-            f.write(i.find('div',{"class" : reg_text}).text)
-            f.write('\n\n++++++++++++\n\n\n')
+
+    if soup.find_all('h2',{'class':'error'}):
+        raise FileNotFound('No poets with the given username')
+    else:
+        with open('composition.txt','w') as f:
+            for i in soup.find_all('div',{"class" : 'fonted'}):
+                f.write(f"{i.a.text.upper()}\n")
+                f.write(i.find('div',{"class" : reg_text}).text)
+                f.write('\n\n++++++++++++\n\n\n')
         
     
 
